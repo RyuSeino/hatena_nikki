bcc6b0163f875a5f838f8f142364ab5900e49e21
Ryu Seino <huntinghighandlow.rs@gmail.com>
2018-02-08 17:20:11 +0900

    fix

--- content.js
+++
@@ -1,9 +0,0 @@
-
-var date = new Date();
-var title = date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日の日記';
-var target = document.getElementById('title');
-
-target.onfocus = function() {
-	document.getElementById('title').value = title;
-};
-
