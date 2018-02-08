7977150e92080ccd4ac245d269a2b368db894bf4
Ryu Seino <huntinghighandlow.rs@gmail.com>
2018-02-08 16:53:27 +0900

    insert date when "title" focused

--- content.js
+++ content.js
@@ -1,17 +1,9 @@
 
-var iframe = document.getElementById('body_ifr');
 var date = new Date();
 var title = date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日の日記';
+var target = document.getElementById('title');
 
-if (!iframe) {
-	var textarea = document.getElementById('body');
-
-	textarea.onfocus = function() {
-	    document.getElementById('title').value = title;
-	}
-} else {
-	iframe.contentDocument.body.onfocus = function() {
-	    document.getElementById('title').value = title;
-	}
-}
+target.onfocus = function() {
+	document.getElementById('title').value = title;
+};
 
